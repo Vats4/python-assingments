a217a8abd8e3f9df81a2cd56d4d141fb0c474579
Vats4 <151975860+Vats4@users.noreply.github.com>
2024-01-15 16:25:51 -0400

    calculator.py

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
-first =input("enter first number: ")
-operator = input("enter operator(+,-,*,/,%): ")
-second = input("enter second number : ")
+first =input ("enter first number: ")
+operator = input ("enter operator(+,-,*,/,%): ")
+second = input ("enter second number : ")
 
 first = int(first)
 second = int(second)
